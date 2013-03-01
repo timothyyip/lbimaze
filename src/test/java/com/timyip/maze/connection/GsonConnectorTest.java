200e77f8c59084bbc9d8df97c521e642182f838f
Timothy Yip <tim@timothyyip.org>
2013-03-01 06:56:59 +0000

    location type enum tested

--- src/test/java/com/timyip/maze/connection/GsonConnectorTest.java
+++ src/test/java/com/timyip/maze/connection/GsonConnectorTest.java
@@ -4,9 +4,12 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class GsonConnectorTest {
 
@@ -27,6 +30,17 @@ public class GsonConnectorTest {
     @Test
     public void canGetCurrentLocationType() throws IOException {
         assertNotNull(gsonConnector.start().getLocationType());
+    }
+
+    @Test
+    public void locationTypeIsOneOfTheFourInput() throws IOException {
+        List<String> possibleLocationType = new ArrayList<String>();
+        possibleLocationType.add("Start");
+        possibleLocationType.add("Normal");
+        possibleLocationType.add("Exit");
+        possibleLocationType.add("PowerPill");
+
+        assertTrue(possibleLocationType.contains(gsonConnector.start().getLocationType()));
     }
 
 
